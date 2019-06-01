0af48376be51e7e11da2527daf5e9c34b5f06ec5
arunjohnkuruvilla <arunjohnkuruvilla@gmail.com>
2019-06-01 13:57:50 -0700

    Completed challenge 13

--- challenge9.py
+++ challenge9.py
@@ -6,20 +6,20 @@ def pkcs7_pad(data, block_size):
     remaining_bytes = block_size - len(data)%block_size
 
     for x in xrange(0, remaining_bytes):
-        data = data + struct.pack('B', 4)
+        data = data + struct.pack('B', remaining_bytes)
     return data
 
 def pkcs7_unpad(data):
 
-    padding = data[-struct.unpack('B', data[-1])[0]:]
+    padding = data[-struct.unpack('B', str(data[-1]))[0]:]
 
-    if all(padding[b] == len(padding) for b in xrange(0, len(padding))):
+    if all(ord(padding[b]) == len(padding) for b in xrange(0, len(padding))):
         return data[:-len(padding)]
     else:
         return data
 
 def main():
-    unpadded_data = bytearray("YELLOW SUBMARINE")
+    unpadded_data = r"YELLOW SUBMARINE"
 
     padded_data = pkcs7_pad(unpadded_data, 20)
 
